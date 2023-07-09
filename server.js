7dbfd528654526649d9328d8017d422ce2f4ebea
PedroHPLopes <pedrohplopes98@gmail.com>
2023-07-09 20:03:18 -0300

    Changed public to docs folder

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 // Define routes for login, reset, and items pages
 
